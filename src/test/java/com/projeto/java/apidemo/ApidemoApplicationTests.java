f76f5dd5de39ba5a66e5658b5723f6e294f73735
fapdev77 <fapdev77@gmail.com>
2023-05-01 14:23:15 -0300

    Initial api java spring sample

---
+++ src/test/java/com/projeto/java/apidemo/ApidemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.java.apidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApidemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
